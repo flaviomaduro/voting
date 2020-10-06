2c6f2d42e27bcea570658e8da84f5627c64e169c
flaviomaduro <famaduro@gmail.com>
2020-10-06 09:37:14 -0300

    firt tests ok

--- config/test.exs
+++ config/test.exs
@@ -7,7 +7,7 @@ use Mix.Config
 # Run `mix help test` for more information.
 config :voting, Voting.Repo,
   username: "postgres",
-  password: "postgres",
+  password: "ur82060",
   database: "voting_test#{System.get_env("MIX_TEST_PARTITION")}",
   hostname: "localhost",
   pool: Ecto.Adapters.SQL.Sandbox
--- mix.exs
+++ mix.exs
@@ -43,7 +43,7 @@ defmodule Voting.MixProject do
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
       {:plug_cowboy, "~> 2.0"},
-      {:credo, "~> 1.5.0-rc.2", only: [:dev, :test], runtime: false},
+      {:credo, "~> 1.5.0-rc.2", only: [:dev, :test], runtime: false}
     ]
   end
 
